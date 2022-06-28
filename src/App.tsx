322507ebba8f33c6cb84a340935b4f246a1b50da
arian <kapqworks@gmail.com>
2022-06-28 17:34:29 +0430

    blockout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
 import React from "react";
+import { LandingPage } from "./Components/LandingPage";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    BrowserRouter
+} from "react-router-dom";
+import { DetailPage } from "./Components/DetailPage";
+import "./main.css"
 
 const App = () => {
-    return (
-        <div>App</div>
-    )
-}
 
+    return (
+        <>
+            <div className="header">
+                Title
+            </div>
+
+            <div className="body">
+                <LandingPage />
+                <LandingPage />
+                <LandingPage />
+            </div>
+
+            <div className="footer">
+                Footer
+            </div>
+        </>
+    )
+    // return (
+    //     <BrowserRouter>
+    //                 <div>route test</div>
+    //                 <div>
+    //                     <ul>
+    //                         <li><Link to={"/"} >Home</Link></li>
+    //                         <li><Link to={"/about"} >about</Link></li>
+    //                         <li><Link to={"/test"} >test</Link></li>
+    //                     </ul>
+    //                 </div>
+
+    //                 <Routes>
+    //                     <Route path="/" element={<Home />} />
+    //                     <Route path="/about" element={<About />} />
+    //                     <Route path="/test" element={<Test />} />
+    //                 </Routes>
+    //             </BrowserRouter>
+    // )
+}
 export default App
---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Css/LandingPage.css"
+
+export const LandingPage = () => {
+    return (
+        <div className="content">
+            <div className="box"></div>
+            <h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            </h3>
+        </div>
+    )
+}
+
